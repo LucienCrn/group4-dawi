77fff9c7e91f39258d02033fe9ac810cd0510c9c
Vince161 <vincent.rabussier@gmail.com>
2020-06-11 18:51:39 +0200

    Etudiant View

--- views/entreprise_view.php
+++ views/entreprise_view.php
@@ -15,7 +15,7 @@ echo('');
 </head>
 
 <div>
-  <a href=http://localhost/group4-dawi/views/entreprise_view.php><img src="https://i.imgur.com/ZuVOp2H.png" title="source: imgur.com" alt="custom_html_banner1" style="width:100%"/></a>
+  <a href=http://localhost/group4-dawi/views/entreprise_view.php><img src="https://i.imgur.com/ZuVOp2H.png" title="Espace Entreprise" alt="custom_html_banner1" style="width:100%"/></a>
 </div>
 
 <body>
@@ -24,47 +24,50 @@ echo('');
 
 <h1><span id="projet_tuteure"></span>Projet Tuteuré :</h1>
 
-<section><div style='padding-left:100px;'><h4>Ajouter un projet tuteuré :</h4>
+<section><div style='padding-left:100px;'><h4>Ajouter un projet tuteuré :</h4></div>
 
-<p style='margin-right:200px;'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+<p style='margin-left:200px; margin-right:200px;'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
 magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
 in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-culpa qui officia deserunt mollit anim id est laborum."</p></div></section>
+culpa qui officia deserunt mollit anim id est laborum."</p></section>
 
 <div style="text-align:center;"><button style="color: #FFFFFF; background-color: #00365B; border: none; padding: 15px 32px; text-align: center; 
 font-size: 16px; margin: 4px 2px; cursor: pointer;" type="button">Insérez PDF</button></div> 
 
-<section><div style='padding-left:100px;'><h4>Consulter ses projets tuteurés :</h4>
+<section><div style='padding-left:100px;'><h4>Consulter ses projets tuteurés :</h4></div>
 
-<p style='margin-right:200px;'>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+<p style='margin-left:200px; margin-right:200px;'>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
 eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
 sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, 
 qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam 
 aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea 
 commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem 
-eum fugiat quo voluptas nulla pariatur?"</p></div></section>
+eum fugiat quo voluptas nulla pariatur?"</p></section>
 
 <div style='padding-bottom:10px;'></div>
 
-<div style='padding-bottom:10px; margin-top:50px;'><h1><span id="support_de_cours"></span>Offre d'alternance : </h1>
+<h1><span id="offre_alternance"></span>Offre d'alternance : </h1>
 
-<section><div style='padding-left:100px;'><h4>Ajouter une offre d'alternance :</h4>
+<section><div style='padding-left:100px;'><h4>Ajouter une offre d'alternance :</h4></div>
 
-<p style='margin-right:200px;'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+<p style='margin-left:200px; margin-right:200px;'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
 magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
 in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-culpa qui officia deserunt mollit anim id est laborum."</p></div></section>
+culpa qui officia deserunt mollit anim id est laborum."</p></section>
 
 <div style="text-align:center;"><button style="color: #FFFFFF; background-color: #E06F24; border: none; padding: 15px 32px; text-align: center; 
 font-size: 16px; margin: 4px 2px; cursor: pointer;" type="button">Insérez PDF</button></div>
 
-<section><div style='padding-left:100px;'><h4>Consulter ses offres d'alternances :</h4>
+<section><div style='padding-left:100px;'><h4>Consulter ses offres d'alternances :</h4></div>
 
-<p style='margin-right:200px;'>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+<p style='margin-left:200px; margin-right:200px;'>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
 eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
 sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, 
 qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam 
 aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea 
 commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem 
-eum fugiat quo voluptas nulla pariatur?"</p></div></section>
+eum fugiat quo voluptas nulla pariatur?"</p></section>
 
+</body>
+
+</html>
