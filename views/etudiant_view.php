074ee7afea2669608fe89238b7d4a37fd88ee132
Vince161 <vincent.rabussier@gmail.com>
2020-06-11 18:51:11 +0200

    Etudiant View

---
+++ views/etudiant_view.php
@@ -0,0 +1,53 @@
+<?php
+
+echo('');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='Content-Type' content='text/html;'>
+    <meta name='author' content='R.Sites Project'>
+    <!-- <link href='assets/styles/main.css' rel='stylesheet'>
+    <link href='assets/images/logo.png' rel="icon"> -->
+    <title>Espace Etudiant</title>
+</head>
+
+<div>
+  <a href=http://localhost/group4-dawi/views/etudiant_view.php><img src="https://i.imgur.com/XI3xy73.png" title="Espace Etudiant" alt="custom_html_banner2" style="width:100%"/></a>
+</div>
+
+<body>
+
+<div style='padding-bottom:10px;'></div>
+
+<h1><span id="projets_tuteures"></span>Projets Tuteurés :</h1>
+
+<p style='margin-left:200px; margin-right:200px;'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+culpa qui officia deserunt mollit anim id est laborum."</p></section>
+
+<div style='padding-bottom:10px;'></div>
+
+<h1><span id="offres_alternances"></span>Offres d'alternances : </h1>
+
+<p style='margin-left:200px; margin-right:200px;'>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, 
+qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam 
+aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea 
+commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem 
+eum fugiat quo voluptas nulla pariatur?"</p></section>
+
+<div style='padding-bottom:10px;'></div>
+
+<h1><span id="support_de_cours"></span>Support de cours : </h1>
+
+<div style="text-align:center;"><button style="color: #FFFFFF; background-color: #E06F24; border: none; padding: 15px 32px; text-align: center; 
+font-size: 16px; margin: 4px 2px; cursor: pointer;" type="button">Télécharger Support</button></div>
+
+</body>
+
+</html>
