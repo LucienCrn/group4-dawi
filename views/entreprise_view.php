8b1ef66754a0b2246b5086e80735f4b06ae141d0
Vince161 <vincent.rabussier@gmail.com>
2020-06-12 15:54:21 +0200

    Vue Entreprise 12

--- views/entreprise_view.php
+++ views/entreprise_view.php
@@ -37,6 +37,11 @@ include_once("views/includes/header.php");
         <div class="text-center">
             <iframe width="70%" height="300" src="assets/img/dummyPtEl.pdf"></iframe>
         </div>
+
+        <a href="assets/img/dummyPtEl.pdf"><div style="text-align:center;"><button style="color: #FFFFFF; background-color: #E06F24; 
+        border: none; padding: 15px 32px; text-align: center; font-size: 16px; margin: 4px 2px; margin-top: 50px; margin-bottom: 20px; cursor: pointer;" 
+        type="button">Télécharger Projet Tuteuré</button></div></a>
+
     </div>
 </div>
 
@@ -63,6 +68,11 @@ include_once("views/includes/header.php");
         <div class="text-center" id="mrg">
             <iframe width="70%" height="300" src="assets/img/dummyOaEl.pdf"></iframe>
         </div>
+
+        <a href="assets/img/dummyOaEl.pdf"><div style="text-align:center;"><button style="color: #FFFFFF; background-color: #00365B; 
+        border: none; padding: 15px 32px; text-align: center; font-size: 16px; margin: 4px 2px;  margin-bottom: 20px; cursor: pointer;" 
+        type="button">Télécharger Offres Alternances</button></div></a>
+
     </div>
 </div>
 </div>
