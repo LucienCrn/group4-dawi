4ce9d6406ae99f953d3e6d7d8bac68862618c1b1
Maxime <maxime1.couderc@gmail.com>
2020-06-12 09:27:38 +0200

    Adding SVG pictures to make icon for the website

--- views/includes/head.php
+++ views/includes/head.php
@@ -6,5 +6,5 @@
     <link href='/group4-dawi/assets/css/style.css' rel='stylesheet'>
     <link href='/group4-dawi/assets/Bootstrap/css/bootstrap.min.css' rel='stylesheet'>
     <link href='/group4-dawi/assets/Bootstrap/js/bootstrap.min.js' rel='script'>
-    <!-- <link href='assets/images/logo.png' rel="icon"> -->
+    <link href='assets/images/15457.svg' rel="icon">
 </head>
